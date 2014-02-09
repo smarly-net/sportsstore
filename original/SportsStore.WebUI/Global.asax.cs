31c46375b7858ba445a41f0c5ad5943cec249111
Dmitriev Denis <smarly@smarly.net>
2014-02-09 18:47:27 +0300

    Add NinjectControllerFactory

--- original/SportsStore.WebUI/Global.asax.cs
+++ original/SportsStore.WebUI/Global.asax.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using SportsStore.WebUI.Infrastructure;
 
 namespace SportsStore.WebUI
 {
@@ -16,6 +13,8 @@ namespace SportsStore.WebUI
 			FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
 			RouteConfig.RegisterRoutes(RouteTable.Routes);
 			BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+			ControllerBuilder.Current.SetControllerFactory(new NinjectControllerFactory());
 		}
 	}
 }
