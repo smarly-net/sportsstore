4d676c7c30f1c323272d944451d398768059e14c
Dmitriev Denis <smarly@smarly.net>
2014-02-09 18:54:11 +0300

    Добавляем имитированную реализацию IProductRepository

--- original/SportsStore.WebUI/Infrastructure/NinjectControllerFactory.cs
+++ original/SportsStore.WebUI/Infrastructure/NinjectControllerFactory.cs
@@ -1,7 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Web.Mvc;
 using System.Web.Routing;
+using Moq;
 using Ninject;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
 
 namespace SportsStore.WebUI.Infrastructure
 {
@@ -22,7 +27,15 @@ namespace SportsStore.WebUI.Infrastructure
 		}
 		private void AddBindings()
 		{
-			// put bindings here
+			// Добавляем имитированную реализацию IProductRepository
+			Mock<IProductRepository> mock = new Mock<IProductRepository>();
+			mock.Setup(m => m.Products).Returns(new List<Product> {
+          new Product { Name = "Football", Price = 25 },
+          new Product { Name = "Surf board", Price = 179 },
+          new Product { Name = "Running shoes", Price = 95 }
+        }.AsQueryable());
+
+			ninjectKernel.Bind<IProductRepository>().ToConstant(mock.Object);
 		}
 	}
 }
